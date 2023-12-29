b2f4ed5ed532391f2733fb6520dfbe742c3e7953
Avi Avinav <aviavinav2004@gmail.com>
2023-12-29 14:36:20 +0530

    add basic animation & remove useless logic

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,12 @@ const crossButton = document.getElementById("cross") as HTMLButtonElement;
 const ele = document.getElementById("ele") as HTMLDivElement;
 
 burButton.onclick = () => {
-	if (burButton.style.display === "none") {
-		burButton.style.display = "block";
-		crossButton.style.display = "none";
-		ele.style.display = "none";
-	} else {
-		burButton.style.display = "none";
-		crossButton.style.display = "block";
-		ele.style.display = "flex";
-	}
+	burButton.style.display = "none";
+	crossButton.style.display = "block";
+	ele.style.display = "flex";
 };
 crossButton.onclick = () => {
-	if (crossButton.style.display === "none") {
-		crossButton.style.display = "block";
-		burButton.style.display = "none";
-		ele.style.display = "flex";
-	} else {
-		crossButton.style.display = "none";
-		burButton.style.display = "block";
-		ele.style.display = "none";
-	}
+	crossButton.style.display = "none";
+	burButton.style.display = "block";
+	ele.style.display = "none";
 };
